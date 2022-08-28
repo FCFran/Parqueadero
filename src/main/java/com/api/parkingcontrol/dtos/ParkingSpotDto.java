164a37fa59ab746aaf197e7b5b553ddb4cbc7388
Fran Flores <fcfran_12@hotmail.com>
2022-08-27 20:09:09 -0500

    Nuevo Proyecto Parking Spot

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,103 @@
+package com.api.parkingcontrol.dtos;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class ParkingSpotDto {
+
+
+    @NotBlank
+    private String parkingSpotNumber; //número de lugar de estacionamiento
+
+    @NotBlank
+    private String licensePlateCar; // número de estacionamiento
+
+    @NotBlank
+    @Size(max = 7)
+    private String brandCar; // marca de carro
+
+    @NotBlank
+    private String modelCard;
+
+    @NotBlank
+    private String colorCar;
+
+    @NotBlank
+    private String responsibleName;
+
+    @NotBlank
+    private String apartment;
+
+    @NotBlank
+    private String block;
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getLicensePlateCar() {
+        return licensePlateCar;
+    }
+
+    public void setLicensePlateCar(String licensePlateCar) {
+        this.licensePlateCar = licensePlateCar;
+    }
+
+    public String getBrandCar() {
+        return brandCar;
+    }
+
+    public void setBrandCar(String brandCar) {
+        this.brandCar = brandCar;
+    }
+
+    public String getModelCard() {
+        return modelCard;
+    }
+
+    public void setModelCard(String modelCard) {
+        this.modelCard = modelCard;
+    }
+
+    public String getColorCar() {
+        return colorCar;
+    }
+
+    public void setColorCar(String colorCar) {
+        this.colorCar = colorCar;
+    }
+
+    public String getResponsibleName() {
+        return responsibleName;
+    }
+
+    public void setResponsibleName(String responsibleName) {
+        this.responsibleName = responsibleName;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+}
